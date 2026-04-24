96be59cc65d72fa770f8b462ff25ea62b30c5fd3
0xENDER <72353925+0xENDER@users.noreply.github.com>
2026-04-24 15:16:02 +0300

    Remove failing tests

--- tests/core/parser/tmp.cpp
+++ tests/core/parser/tmp.cpp
@@ -9,14 +9,6 @@ TEST(ParserTest, Temporary1) {
     EXPECT_EQ(1, 1);
 }
 
-TEST(ParserTest, Temporary1_) {
-    EXPECT_EQ(1, 0);
-}
-
 TEST(ParserTest, Temporary2) {
     EXPECT_NE(1, 0);
-}
-
-TEST(ParserTest, Temporary2_) {
-    EXPECT_NE(1, 1);
 }
